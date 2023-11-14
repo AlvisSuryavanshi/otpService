74b8b9a7cb7070eeaa375dbd32123c2c038947a6
AlvisSuryavanshi <alvis.surya@gmail.com>
2023-11-14 17:02:51 +0530

    payload class created (for posting mobile number)

---
+++ src/main/java/com/otpService/payload/OTPRequest.java
@@ -0,0 +1,13 @@
+package com.otpService.payload;
+
+public class OTPRequest {
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
