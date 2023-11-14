17f4a29ec1a1f49ec28baf13a5c8589626b7572e
AlvisSuryavanshi <alvis.surya@gmail.com>
2023-11-14 17:42:03 +0530

    service logic for verify otp

--- src/main/java/com/otpService/service/OTPService.java
+++ src/main/java/com/otpService/service/OTPService.java
@@ -6,6 +6,8 @@ import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Service
@@ -24,7 +26,7 @@ public class OTPService {
 
     // Store generated OTPs in memory (Replace with a database in production)
 
-
+    private final Map<String, String> generatedOtps = new HashMap<>();
     public void sendOtp(String recipientPhoneNumber) {
         Twilio.init(twilioAccountSid, twilioAuthToken);
 
@@ -39,13 +41,26 @@ public class OTPService {
         ).create();
 
         // Store the generated OTP in memory
-
+        generatedOtps.put(recipientPhoneNumber, otp);
     }
 
     private String generateRandomOtp() {
         // Generate a random 6-digit OTP
         int otp = 100_000 + random.nextInt(900_000); // Generates a number between 100,000 and 999,999
         return String.valueOf(otp);
+    }
+
+    public String verifyOtp(String recipientPhoneNumber, String enteredOtp) {
+        // Retrieve the previously generated OTP from memory
+        String generatedOtp = generatedOtps.get(recipientPhoneNumber);
+        boolean b = generatedOtp != null && generatedOtp.equals(enteredOtp);
+        if(b){
+            return "OTP is Confirmed";
+
+        }
+        else return "OTP not verified";
+        // Verify the OTP
+
     }
 }
 
